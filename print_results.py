295186322a0c09e8d7c586f229aa8fe28584a8fb
Fanus Arefaine <fanus.gebrehiwet@gmail.com>
2020-07-11 10:55:07 -0700

    print result mod

--- print_results.py
+++ print_results.py
@@ -1,6 +1,10 @@
+# A function to print results based on the combinations of model parameter
+
 def print_results(results):
+
     print("Best Params: {}\n".format(results.best_params_))   
     means = results.cv_results_['mean_test_score']
     stds = results.cv_results_['std_test_score']
+    
     for mean, std, params in zip(means, stds, results.cv_results_['params']):
         print('{} (+/-{}) for {}'.format(round(mean,3), round(std*2, 3), params))
