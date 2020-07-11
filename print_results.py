26f9f17500c6b2aff5d3b3ce066204c8896a276d
Fanus Arefaine <fanus.gebrehiwet@gmail.com>
2020-07-10 23:55:51 -0700

    file mod

--- print_results.py
+++
@@ -1,6 +0,0 @@
-def print_results(results):
-    print("Best Params: {}\n".format(results.best_params_))   
-    means = results.cv_results_['mean_test_score']
-    stds = results.cv_results_['std_test_score']
-    for mean, std, params in zip(means, stds, results.cv_results_['params']):
-        print('{} (+/-{}) for {}'.format(round(mean,3), round(std*2, 3), params))
