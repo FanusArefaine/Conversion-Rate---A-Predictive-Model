89a14cb8563a51ee7fb63d5a9e8bfa3e9e28141d
Fanus Arefaine <fanus.gebrehiwet@gmail.com>
2020-07-11 10:01:54 -0700

    modified outlier detector

--- outlier_detector.py
+++ outlier_detector.py
@@ -5,28 +5,25 @@ import numpy as np
 
 outliers = list()
 
-class outlier:
-
-    def minoutlier(self, data):
 
 
-        """ data_mean : calculate the mean of the data
-            data_std : calculate the standard deviation of the data
-        """
-
-        data_mean = np.mean(data)
-        data_std = np.std(data)
-
-        for record in data:
-            z_score = (record - data_mean)/ data_std
-            # An outlier if the data record lies beyond the 3rd standard deviation
-            if np.abs(z_score) > 3: 
-                outliers.append(record)
-        return min(outliers)
+def minoutlier(self, data):
 
 
-    def add(self, a, b):
-        return a+b
+    """ data_mean : calculate the mean of the data
+        data_std : calculate the standard deviation of the data
+    """
+
+    data_mean = np.mean(data)
+    data_std = np.std(data)
+
+    for record in data:
+        z_score = (record - data_mean)/ data_std
+        # An outlier if the data record lies beyond the 3rd standard deviation
+        if np.abs(z_score) > 3: 
+            outliers.append(record)
+    return min(outliers)
+
 
         
     
